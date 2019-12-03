71d4582d54b32e4534902d898f999348dc648490
FireWall-e <40099369+FireWall-e@users.noreply.github.com>
2019-12-03 22:56:48 +0200

    Responsivness improvements

--- learnix/src/main.js
+++ learnix/src/main.js
@@ -1,10 +1,11 @@
-const go = {
+console.log('Have a nice day, 👨‍💻 👩‍💻');
+        const go = {
             d: document.createElement('_'),
             eventBindAll: (s, e, f) => {
                 const element = document.querySelectorAll(s);
-			    for (s = 0; s < element.length; s++) {
-				    element[s].addEventListener(e, f);
-			    }
+                for (s = 0; s < element.length; s++) {
+                    element[s].addEventListener(e, f);
+                }
             },
             get: (s) => {
                 return document.querySelector(s);
@@ -25,12 +26,33 @@ const go = {
                         });
                     }
                 });   
-            };
-            _.classList.add('selected');
-            _.parentElement.classList.add('expand');
+           
+                _.classList.add('selected');
+                _.parentElement.classList.add('expand');
 
-            (go.get('.main-content.show') || go.d).classList.remove('show');
-            (go.get(`#${_.dataset.contentId}`) || go.d).classList.add('show');
+                (go.get('.main-content.show') || go.d).classList.remove('show');
+                if (_.dataset.contentId) {
+                    go.getAll('.sidebar').forEach( e => {
+                        e.classList.remove('expand');
+                    });
+                    (go.get(`#${_.dataset.contentId}`) || go.d).classList.add('show');
+                }
+            };
         };
+
+        go.eventBindAll('.sidebar', 'click', e => {
+            e = e.target.parentElement;
+            if (e.classList.contains('sidebar')) e.classList.toggle('expand');
+        });
               
         go.eventBindAll('.menu-link', 'click', navigateMenu);
+
+        go.eventBindAll('.main-title', 'click', () => {
+            (go.get('.main-content.show') || go.d).classList.remove('show');
+            go.getAll('.menu-item.expand').forEach(item => {
+                item.classList.remove('expand');
+                item.childNodes.forEach(link => {
+                    if(link.classList) link.classList.remove('selected');
+                });
+            });  
+        });
