1611e83092eb5dc78812188e63aa6628532f805d
Vladyslav Slipchenko <vladyslav.slipchenko@nixsolutions.com>
2022-04-30 11:10:58 +0200

    Update profile

--- showcase/showpad/src/modules/profile/profile.component.ts
+++ showcase/showpad/src/modules/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {PokemonService} from '~services/pokemon/pokemon.service';
+import {BehaviorSubject, finalize} from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -8,7 +10,20 @@ import {PokemonService} from '~services/pokemon/pokemon.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent implements OnInit {
-  constructor(private readonly pokemonService: PokemonService) {}
+  loading$ = new BehaviorSubject(true);
+  name = '';
+  pokemon: {stats: Array<{name: string; base_stat: number}>; moves: Array<string>} = {} as any;
 
-  ngOnInit(): void {}
+  constructor(private readonly pokemonService: PokemonService, private route: ActivatedRoute) {
+    this.name = (this.route.snapshot.params as {name: string}).name;
+  }
+
+  ngOnInit(): void {
+    this.pokemonService
+      .getPokemonByName('ditto')
+      .pipe(finalize(() => this.loading$.next(false)))
+      .subscribe((pokemon) => {
+        console.log(pokemon);
+      });
+  }
 }
